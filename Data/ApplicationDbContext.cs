bf8a4150f902c4b43e9fa6c858c28618820754a1
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2022-10-06 20:33:54 -0600

    Identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Net6AddressBook.Models;
 
 namespace Net6AddressBook.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
