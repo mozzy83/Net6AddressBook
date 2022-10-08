86c964d53dfffee09ea18adfbbfb6ab207d26e11
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2022-10-07 21:31:37 -0600

    Create Contact and Category models
    Created Contact and Categories models as well as CategoryContact table and migrated to database.

---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Net6AddressBook.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? AppUserId { get; set; }
+        [Required]
+        [Display(Name="Category Name")]
+        public string? Name { get; set; }
+
+        //Virtuals
+        //Create foreign key to AppUser Model
+        public virtual AppUser? AppUser { get; set; }
+        //Creat foreign key to Contacts table. Since its a collection it will create a join table. Must create DbContext for Contacts table in ApplicationDvContext so migration generates database table.
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+
+    }
+}
